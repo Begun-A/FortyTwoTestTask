9600bf0233df319d62a590df51b90d7a384b9db1
DeV1doR <dev1dor@ukr.net>
2015-08-23 17:07:22 +0300

    ticket:3 tests fail

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,13 +1,16 @@
-import unittest
+from django.test import TestCase
 from django.test.client import RequestFactory
 from django.core.urlresolvers import reverse
 
 from apps import initial_data
-from .views import ContactView
+from .views import (
+    ContactView,
+    LogRequestView,
+)
 
 
-class ContactUnitTest(unittest.TestCase):
-    """Simple test for status code.
+class ContactUnitTest(TestCase):
+    """Simple test for status code. Testing '/contact/1/'
     """
 
     def setUp(self):
@@ -16,7 +19,26 @@ class ContactUnitTest(unittest.TestCase):
         self.factory = RequestFactory()
 
     def test_contact_get_ok_request(self):
+        """Check if page get status OK.
+        """
         request = self.factory.get(path=self.fake_path)
         view = ContactView.as_view()
         response = view(request, pk=self.pk)
         self.assertEqual(response.status_code, 200)
+
+
+class LogRequestTest(TestCase):
+    """Check status, ajax work.
+    """
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.fake_path = reverse('requests')
+        self.requests_html = 'hello/requests.html'
+
+    def test_requests_get_ok_request(self):
+        request = self.factory.get(path=self.fake_path)
+        view = LogRequestView.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertHTMLEqual(response.template_name[0], self.requests_html)
