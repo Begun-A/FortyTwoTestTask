32b6fafb8512db92dbe5a0a0402ac47f6891ea04
DeV1doR <dev1dor@ukr.net>
2015-08-25 01:11:35 +0300

    ticket:5 added static patterns

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from hello.views import (
     ContactView,
@@ -9,11 +10,10 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^contact/$', ContactView.as_view(), name='contact'),
     url(r'^requests/$', LogRequestView.as_view(), name='requests'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
